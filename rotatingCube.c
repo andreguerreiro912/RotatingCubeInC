f9c3a0794e804c34d7310d8affa849083e5c4ca0
André Guerreiro <andrefiliperg@gmail.com>
2024-12-24 11:09:06 +0000

    Added point and face

--- rotatingCube.c
+++ rotatingCube.c
@@ -1,31 +1,44 @@
 #include <time.h>
 #include <stdio.h>
 
-// Function to clear the screen
 void clearConsole() {
     printf("\033[H\033[J");
 }
 
+typedef struct {
+    float x, y, z; 
+} Point3D;
+
+typedef struct {
+    Point3D v1;  // First vertex
+    Point3D v2;  // Second vertex
+    Point3D v3;  // Third vertex
+    Point3D v4;  // Fourth vertex
+    char depthChar; // Char that will be used to differentiate the face of the cube
+} Face;
+
 int main() {
 
     struct timespec req;
+    double rotatingVelocityX = 0;
+    double rotatingVelocityY = 0;
+    double rotatingVelocityZ = 0;
 
     // fazer o for loop infinito que vai fazer a animação
 
     int continuar = 1; 
-    while (continuar)
+    /*while (continuar)
     {
     clearConsole();
     req.tv_sec = 0;                 // Seconds
     req.tv_nsec = 200000000L;       // Nanoseconds (200 ms = 200,000,000 ns)
-
 
     // 
 
 
 
     nanosleep(&req, NULL);          // Pause execution for the specified time
-    }
+    }*/
 
     return 0;
 }
